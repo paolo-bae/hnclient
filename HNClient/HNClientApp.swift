fc1f0bc05aa97e0780a2c04c0a44911d906d9753
paolo-bae <paolo.baeli@gmail.com>
2022-07-14 12:52:28 +0200

    First commit

---
+++ HNClient/HNClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNClientApp.swift
+//  HNClient
+//
+//  Created by Paolo Baeli on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HNClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
