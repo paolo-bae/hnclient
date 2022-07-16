443c8ae5500a2c9ec5492053637ef2712b8df8d9
paolo-bae <paolo.baeli@gmail.com>
2022-07-16 16:24:43 +0200

    Almost finished implementing the UI

--- HNClient/Features/Model/StoriesModel.swift
+++ HNClient/Features/Model/StoriesModel.swift
@@ -18,17 +18,58 @@ struct Story: Codable, Identifiable {
     let title: String
     let type: String
     let url: String
+}
     
-    
-    //MARK: Just a sample story for test purposes
-    static var sampleStory = Story(id: 6891,
-                                   by: "domp",
-                                   descendants: 3,
-                                   kids: [6931, 6996],
-                                   score: 7,
-                                   time: 1175083893,
-                                   title: "Vutool: Stealth Startup of a \"Google Earth\" from the ground level",
-                                   type: "story",
-                                   url: "http://www.techcrunch.com/2007/03/28/whispers-about-stealth-startup-vutool/")
+    //MARK: Just a sample array of stories for test purposes
+extension Story {
+    static let dummyStories: [Story] = [
+        Story(                         id: 6891,
+                                       by: "domp",
+                                       descendants: 3,
+                                       kids: [6931, 6996],
+                                       score: 7,
+                                       time: 1175083893,
+                                       title: "Vutool: Stealth Startup of a \"Google Earth\" from the ground level",
+                                       type: "story",
+                                       url: "http://www.techcrunch.com/2007/03/28/whispers-about-stealth-startup-vutool/"),
+        Story(                         id: 6891,
+                                       by: "domp",
+                                       descendants: 3,
+                                       kids: [6931, 6996],
+                                       score: 7,
+                                       time: 1175083893,
+                                       title: "Vutool: Stealth Startup of a \"Google Earth\" from the ground level",
+                                       type: "story",
+                                       url: "http://www.techcrunch.com/2007/03/28/whispers-about-stealth-startup-vutool/"),
+        Story(                         id: 6891,
+                                       by: "domp",
+                                       descendants: 3,
+                                       kids: [6931, 6996],
+                                       score: 7,
+                                       time: 1175083893,
+                                       title: "Vutool: Stealth Startup of a \"Google Earth\" from the ground level",
+                                       type: "story",
+                                       url: "http://www.techcrunch.com/2007/03/28/whispers-about-stealth-startup-vutool/"),
+        Story(                         id: 6891,
+                                       by: "domp",
+                                       descendants: 3,
+                                       kids: [6931, 6996],
+                                       score: 7,
+                                       time: 1175083893,
+                                       title: "Vutool: Stealth Startup of a \"Google Earth\" from the ground level",
+                                       type: "story",
+                                       url: "http://www.techcrunch.com/2007/03/28/whispers-about-stealth-startup-vutool/"),
+        Story(                         id: 6891,
+                                       by: "domp",
+                                       descendants: 3,
+                                       kids: [6931, 6996],
+                                       score: 7,
+                                       time: 1175083893,
+                                       title: "Vutool: Stealth Startup of a \"Google Earth\" from the ground level",
+                                       type: "story",
+                                       url: "http://www.techcrunch.com/2007/03/28/whispers-about-stealth-startup-vutool/"),
+        
+        
+    ]
 }
 
--- HNClient/Features/Views/NewStoriesTabView.swift
+++ HNClient/Features/Views/NewStoriesTabView.swift
@@ -11,7 +11,19 @@ struct NewStoriesTabView: View {
     var body: some View {
         NavigationView {
             List {
-                
+                ForEach(Story.dummyStories, id: \.id) { item in
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(alignment: .leading) {
+                            Image(systemName: "papernews")
+                            Text(item.title)
+                        }
+                        HStack {
+                            Text("By: \(item.by)")
+                            Spacer()
+                            Text("UpVotes: \(item.score)")
+                        }
+                    }
+                }
             }
             .navigationTitle("New stories")
         }
